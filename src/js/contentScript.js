779e6e23170c21d49e78b6367537df804cec9e7d
Jerry Liu <jerryliu55@gmail.com>
2018-09-12 13:51:35 -0400

    WIP

---
+++ src/js/contentScript.js
@@ -0,0 +1,11 @@
+// gets all text without html tags
+function getText(){
+    return document.body.innerText
+}
+
+// gets entire html of page
+function getHTML(){
+    return document.body.outerHTML
+}
+
+console.log(getText());
