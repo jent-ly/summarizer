ccbcd3ec4627e39c4022d9e3e6677fc7f61b6413
Kevin Peng <kevin@kpeng.ca>
2018-09-22 22:49:06 -0400

    testing mashape with my domain

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -9,4 +9,24 @@ function getHTML(){
 }
 
 console.log(getText());
-console.log(getHTML());
+// console.log(getHTML());
+chrome.storage.sync.get({
+    apiKey: ""
+  }, function(items) {
+    console.log(items.apiKey);
+    fetch('https://textanalysis-text-summarization.p.mashape.com/text-summarizer', {
+        method: "POST",
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/x-www-form-urlencoded",
+            "X-Mashape-Key": items.apiKey,
+        },
+        body: {
+          "url": "http://en.wikipedia.org/wiki/Automatic_summarizationfet",
+          "text": "",
+          "sentnum": "5",
+        },
+    }).then(response => {
+        console.log(response);
+    });
+  });
