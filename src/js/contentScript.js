09c29b5ad2ed2bc364e11370d701b13c8befdf1a
Kevin Peng <kevin@kpeng.ca>
2018-09-23 12:51:21 -0400

    api

--- src/js/contentScript.js
+++ src/js/contentScript.js
@@ -8,25 +8,26 @@ function getHTML(){
     return document.body.outerHTML
 }
 
-console.log(getText());
+// console.log(getText());
 // console.log(getHTML());
+var url;
+chrome.tabs.getSelected(null,function(tab) {
+  var url = new URL(tab.url);
+  console.log(url);
+});
 chrome.storage.sync.get({
     apiKey: ""
   }, function(items) {
-    console.log(items.apiKey);
-    fetch('https://textanalysis-text-summarization.p.mashape.com/text-summarizer', {
-        method: "POST",
-        headers: {
-            "Accept": "application/json",
-            "Content-Type": "application/x-www-form-urlencoded",
-            "X-Mashape-Key": items.apiKey,
-        },
-        body: {
-          "url": "http://en.wikipedia.org/wiki/Automatic_summarizationfet",
-          "text": "",
-          "sentnum": "5",
-        },
-    }).then(response => {
-        console.log(response);
-    });
+      console.log(items.apiKey);
+      fetch('https://textanalysis-text-summarization.p.mashape.com/text-summarizer', {
+          method: "POST",
+          headers: {
+              "Accept": "application/json",
+              "Content-Type": "application/json",
+              "X-Mashape-Key": items.apiKey,
+          },
+          body: '{"url":"http://en.wikipedia.org/wiki/Automatic_summarization","text":"","sentnum":8}',
+      }).then(response => {
+          console.log(response);
+      });
   });
